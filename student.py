0fe2985d5462e1f6e9453b044d2919d27e59a890
Papdi Gergo <papdi.gergo@diak.szbi-pg.hu>
2025-05-19 09:45:20 +0200

    learn metodus
    skibidios retes

--- student.py
+++ student.py
@@ -4,12 +4,21 @@ class Student:
     sex = ""
     score = 0
 
+    def introduce(self):
+        print(f"Név: {self.name}, Kor: {self.age}, Pontszám: {self.score}")
+
+    def learn(self, points):
+        self.score += points
+
 tivadar = Student()
-print(tivadar)
+#print(tivadar)
 
 tivadar.name = "El Tivadar"
 tivadar.age = 16
 tivadar.sex = "Trippi Troppi"
 tivadar.score = 20
 
-print(f"Név: {tivadar.name}, Kor: {tivadar.age}, Nem: {tivadar.sex}, Pontszám: {tivadar.score}")
+#print(f"Név: {tivadar.name}, Kor: {tivadar.age}, Pontszám: {tivadar.score}")
+tivadar.introduce()
+tivadar.learn(12)
+tivadar.introduce()
